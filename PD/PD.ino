1de58ff08d8a88bdbf80f67d11653abed8ba41bb
blkwg <84122530+blkwg@users.noreply.github.com>
2021-12-19 16:27:50 +0900

    Update PD.ino

--- PD/PD.ino
+++ PD/PD.ino
@@ -1,18 +1,18 @@
-#include <Servo.h>     // [20213066] 서보모터 라이브러리 사용
+#include <Servo.h>     // 서보모터 라이브러리 사용
 
 /////////////////////////////
 // Configurable parameters //
 /////////////////////////////
 
 // Arduino pin assignment
-#define PIN_LED 9          // [20200000] LED 핀 정의
-#define PIN_SERVO 10       // [20213069] servo 핀 정의
-#define PIN_IR A0      // [20213077] analong 핀 정의 
+#define PIN_LED 9          // LED 핀 정의
+#define PIN_SERVO 10       // servo 핀 정의
+#define PIN_IR A0      //  analong 핀 정의 
 
 // Framework setting
-#define _DIST_TARGET 255  // [20213080] 멈추려는 목표거리 정의
-#define _DIST_MIN 100 // [20213058] 측정 최소거리, 플로터 범위
-#define _DIST_MAX 410    // [20213065] 측정 최대거리,플로터 범위
+#define _DIST_TARGET 255  // 멈추려는 목표거리 정의
+#define _DIST_MIN 100 // 측정 최소거리, 플로터 범위
+#define _DIST_MAX 410    //  측정 최대거리,플로터 범위
 
 // Distance sensor  
 #define _DIST_ALPHA 0.4
@@ -25,16 +25,16 @@
 #define _DUTY_MAX 1565    // 서보 duty 최댓값
 
 // Servo speed control
-#define _SERVO_ANGLE 30       // [20213062] 서보 각도
-#define _SERVO_SPEED 60     // [20213075] 서보의 속도
+#define _SERVO_ANGLE 30       // 서보 각도
+#define _SERVO_SPEED 60     //  서보의 속도
   
 // Event periods
-#define _INTERVAL_DIST 25   // [20213058] 거리측정 주기
-#define _INTERVAL_SERVO 20    // [20213055] 서보 조정 주기  
-#define _INTERVAL_SERIAL 100  // [20213058] 시리얼 제어 주기
+#define _INTERVAL_DIST 25   //거리측정 주기
+#define _INTERVAL_SERVO 20    // 서보 조정 주기  
+#define _INTERVAL_SERIAL 100  //  시리얼 제어 주기
 
 // PID parameters
-#define _KP 0.5   // [20213058]비례이득
+#define _KP 0.5   //비례이득
 #define _KD 60     // 미분이득
 #define _KI 0
 
@@ -46,19 +46,19 @@
 //////////////////////
 
 // Servo instance 
-Servo myservo;    // [20213068] 서보 변수명을 myservo로 선언
+Servo myservo;    // 서보 변수명을 myservo로 선언
 
 // Distance sensor
 float dist_target; // location to send the ball
-float dist_raw, dist_ema;    // [20213077] 측정값, ema필터 적용값
+float dist_raw, dist_ema;    // 측정값, ema필터 적용값
 
 // Event periods
 unsigned long last_sampling_time_dist, last_sampling_time_servo, last_sampling_time_serial; 
-bool event_dist, event_servo, event_serial; // [20213072] 거리, 서보, 시리얼 측정 여부
+bool event_dist, event_servo, event_serial; //거리, 서보, 시리얼 측정 여부
 
 // Servo speed control
-int duty_chg_per_interval; // [20213058]한 주기 당 제어할 최대 duty값
-int duty_target, duty_curr; // [20213055] 목표 pulse주기값, 현재 pulse주기값
+int duty_chg_per_interval; // 한 주기 당 제어할 최대 duty값
+int duty_target, duty_curr; // 목표 pulse주기값, 현재 pulse주기값
 
 // PID variables
 float error_curr, error_prev, control, pterm, dterm, iterm;
@@ -109,25 +109,25 @@ pinMode(PIN_LED,OUTPUT);
 myservo.attach(PIN_SERVO);
 
 // move servo to neutral position
-duty_target = duty_curr = _DUTY_NEU; // [20213075] 초기화
-myservo.writeMicroseconds(duty_curr); // [20213078] 서보 중립위치
+duty_target = duty_curr = _DUTY_NEU; // 초기화
+myservo.writeMicroseconds(duty_curr); // 서보 중립위치
 
 // initialize serial port
-Serial.begin(57600); // [20213058]
+Serial.begin(57600); // 
 
 // initialize global variables 
 dist_target = _DIST_TARGET;
 dist_raw = dist_ema = ir_distance();
 error_curr = error_prev = dist_target - dist_raw;
 
-last_sampling_time_dist = 0; // [20213078] last_sampling_time_dist 초기화
-last_sampling_time_servo = 0; // [20213055]
-last_sampling_time_serial = 0; // [20213080]
+last_sampling_time_dist = 0; // last_sampling_time_dist 초기화
+last_sampling_time_servo = 0; // 
+last_sampling_time_serial = 0; // 
 
 event_dist = event_servo = event_serial = false;
 
 // convert angle speed into duty change per interval.
-  duty_chg_per_interval = (float)(_DUTY_MAX - _DUTY_MIN) * _SERVO_SPEED / ((float)_SERVO_ANGLE) * _INTERVAL_SERVO / 1000; // [20213078] 한 주기 당 제어할 최대 duty값 초기화
+  duty_chg_per_interval = (float)(_DUTY_MAX - _DUTY_MIN) * _SERVO_SPEED / ((float)_SERVO_ANGLE) * _INTERVAL_SERVO / 1000; //  한 주기 당 제어할 최대 duty값 초기화
 }
   
 
@@ -149,7 +149,7 @@ void loop() {
         last_sampling_time_serial += _INTERVAL_SERIAL;
         event_serial = true;
   } 
-//[20213080]
+
 
 
 ////////////////////
@@ -157,39 +157,39 @@ void loop() {
 ////////////////////
 
   if(event_dist) {
-     event_dist = false; //[20213078]
+     event_dist = false; 
   // get a distance reading from the distance sensor
-      dist_raw = ir_distance_filtered(); // [20213055]
-      dist_ema = (1 - _DIST_ALPHA) * dist_ema + _DIST_ALPHA * dist_raw;  //[20213075]
+      dist_raw = ir_distance_filtered(); 
+      dist_ema = (1 - _DIST_ALPHA) * dist_ema + _DIST_ALPHA * dist_raw;  
 
   // PID control logic
-    error_curr = dist_target - dist_ema; // [20213075]
-    pterm = _KP * error_curr; // [20213078]
-    dterm = _KD * (error_curr - error_prev);  //[20213077]
+    error_curr = dist_target - dist_ema; 
+    pterm = _KP * error_curr; 
+    dterm = _KD * (error_curr - error_prev);  
     iterm = _KI * error_curr;
 
     if(abs(iterm) > _ITERM_MAX) iterm = iterm/2;
     if(iterm > _ITERM_MAX) iterm = _ITERM_MAX;
     if(iterm < -_ITERM_MAX) iterm = -_ITERM_MAX;
     
-    control = pterm + dterm + iterm; // [20213078]
+    control = pterm + dterm + iterm; 
 
   // duty_target = f(duty_neutral, control)
-    duty_target = _DUTY_NEU + control; // [20213055]
+    duty_target = _DUTY_NEU + control; 
 
   // keep duty_target value within the range of [_DUTY_MIN, _DUTY_MAX]
   if(duty_target < _DUTY_MIN){
     duty_target = _DUTY_MIN;
-  } // [20213080]
+  } 
   if(duty_target > _DUTY_MAX){
     duty_target = _DUTY_MAX;
-  } // [20213080]
+  }
 
    error_prev = error_curr;  // error update
   }
   
   if(event_servo) {
-event_servo = false; // [20213078]
+event_servo = false; 
 
     // adjust duty_curr toward duty_target by duty_chg_per_interval
 
@@ -210,11 +210,11 @@ event_servo = false; // [20213078]
   
 }
   // update servo position
-  myservo.writeMicroseconds(duty_curr); // [20213058]
+  myservo.writeMicroseconds(duty_curr); 
   
 
   if(event_serial) {
-    event_serial = false; // [20213078]
+    event_serial = false; 
     Serial.print("dist_ir:");
     Serial.print(dist_ema);
     Serial.print(",pterm:");
@@ -229,7 +229,7 @@ event_servo = false; // [20213078]
     Serial.print(duty_chg_per_interval);
     Serial.print(",duty_curr:");
     Serial.print(map(duty_curr,1000,2000,410,510));
-    Serial.println(",Min:100,Low:200,dist_target:255,High:310,Max:410"); // [20213071] 
+    Serial.println(",Min:100,Low:200,dist_target:255,High:310,Max:410"); 
 // Min :측정 최소거리, Max : 최대거리, Low : 목표구역 최소거리, dist_target :기준이 되는 거리, High : 목표구역 최대거리 값을 시리얼 모니터에 표시
 
   }
